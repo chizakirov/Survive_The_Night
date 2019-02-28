43a5b1897acca1e87a8f545051d77f0ef7d1001f
Chi Nguyen <nguyen.chi.byu@gmail.com>
2019-02-28 13:37:17 -0800

    first commit

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    //static belong to the class itself,not to the instance
+    public static int score;
+
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
